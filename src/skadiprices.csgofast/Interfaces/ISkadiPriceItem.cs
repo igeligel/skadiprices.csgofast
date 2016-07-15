fd6e46f9d57840262ccf36a4429b8c0fb0dda613
igeligel <kevinigeligeligel@gmail.com>
2016-07-15 22:29:07 +0200

    docs(interfaces)
    add xml documentation to the classes

--- src/skadiprices.csgofast/Interfaces/ISkadiPriceItem.cs
+++ src/skadiprices.csgofast/Interfaces/ISkadiPriceItem.cs
@@ -1,8 +1,18 @@
 namespace skadiprices.csgofast.Interfaces
 {
+    /// <summary>
+    /// Interface for providing items via the price api.
+    /// </summary>
     public interface ISkadiPriceItem
     {
+        /// <summary>
+        /// Market Hash Name provided by steam for the item.
+        /// </summary>
         string MarketHashName { get; set; }
+        
+        /// <summary>
+        /// Price provided by the pricing api.
+        /// </summary>
         decimal Price { get; set; }
     }
 }
