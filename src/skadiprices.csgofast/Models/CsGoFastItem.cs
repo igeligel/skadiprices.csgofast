c7f3f0ca838d8545529a1cd587b92b0bb3eeea7e
igeligel <kevinigeligeligel@gmail.com>
2016-07-15 22:29:34 +0200

    docs(models)
    add xml documentation to models

--- src/skadiprices.csgofast/Models/CsGoFastItem.cs
+++ src/skadiprices.csgofast/Models/CsGoFastItem.cs
@@ -2,9 +2,20 @@ using skadiprices.csgofast.Interfaces;
 
 namespace skadiprices.csgofast.Models
 {
+    /// <summary>
+    /// Implementation of the ISkadiPriceItem interface.
+    /// Lookup: <see cref="ISkadiPriceItem"/>.
+    /// </summary>
     public class CsGoFastItem : ISkadiPriceItem
     {
+        /// <summary>
+        /// Market Hash Name given by csgofast.
+        /// </summary>
         public string MarketHashName { get; set; }
+
+        /// <summary>
+        /// Price given by csgofast.
+        /// </summary>
         public decimal Price { get; set; }
     }
 }
--- src/skadiprices.csgofast/Models/PriceList.cs
+++ src/skadiprices.csgofast/Models/PriceList.cs
@@ -3,8 +3,15 @@ using skadiprices.csgofast.Interfaces;
 
 namespace skadiprices.csgofast.Models
 {
+    /// <summary>
+    /// Implementation of the SkadiPriceList interface.
+    /// Lookup: <see cref="ISkadiPriceList"/>.
+    /// </summary>
     public class PriceList: ISkadiPriceList
     {
+        /// <summary>
+        /// List containing the items of csgofast.
+        /// </summary>
         public List<ISkadiPriceItem> Items { get; set; }
     }
 }
