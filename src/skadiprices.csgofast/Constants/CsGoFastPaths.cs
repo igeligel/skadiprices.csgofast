a8c712184e1bab01b88e96356193b9b8f2fc6c1e
igeligel <kevinigeligeligel@gmail.com>
2016-07-15 16:54:14 +0200

    docs(constants)
    add xml documentation for methods

--- src/skadiprices.csgofast/Constants/CsGoFastPaths.cs
+++ src/skadiprices.csgofast/Constants/CsGoFastPaths.cs
@@ -1,7 +1,14 @@
 namespace skadiprices.csgofast.Constants
 {
+    /// <summary>
+    /// Class which will include all paths of the api used.
+    /// </summary>
     internal class CsGoFastPaths
     {
+        /// <summary>
+        /// Path for getting prices for all skins given by
+        /// csgofast.com.
+        /// </summary>
         internal const string AllPrices = "/sih/all";
     }
 }
--- src/skadiprices.csgofast/Constants/Uris.cs
+++ src/skadiprices.csgofast/Constants/Uris.cs
@@ -2,8 +2,14 @@ using System;
 
 namespace skadiprices.csgofast.Constants
 {
+    /// <summary>
+    /// Class which has all uris used in the package.
+    /// </summary>
     internal static class Uris
     {
+        /// <summary>
+        /// Standard uri for csgofast.com. It is using HTTPS.
+        /// </summary>
         internal static Uri CsGoFastSecured = new Uri("https://api.csgofast.com");
     }
 }
